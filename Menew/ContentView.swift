dc8e50478e5c44087821d5acd4fa0be3903cf42d
Luca Beetz <luca-beetz@tutanota.com>
2022-02-18 13:07:12 +0100

    Initial Commit

---
+++ Menew/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Menew
+//
+//  Created by Luca Beetz on 18.02.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Menew/MenewApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenewApp.swift
+//  Menew
+//
+//  Created by Luca Beetz on 18.02.22.
+//
+
+import SwiftUI
+
+@main
+struct MenewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
