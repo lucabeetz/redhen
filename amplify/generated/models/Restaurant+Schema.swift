28edc48f0bd0984beffa7e89b1b0f52cf7723bea
Luca Beetz <luca-beetz@tutanota.com>
2022-02-21 13:15:56 +0100

    Fix deprecation warnings

--- amplify/generated/models/Restaurant+Schema.swift
+++ amplify/generated/models/Restaurant+Schema.swift
@@ -19,7 +19,7 @@ extension Restaurant {
   public static let schema = defineSchema { model in
     let restaurant = Restaurant.keys
     
-    model.pluralName = "Restaurants"
+    model.syncPluralName = "Restaurants"
     
     model.fields(
       .id(),
