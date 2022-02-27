c647cfa38bc4f76e2536afb33fa5b32a11fdde53
Luca Beetz <luca-beetz@tutanota.com>
2022-02-27 13:57:22 +0100

    Add new restaurant pin designs: open/closed, AR, notification

--- redhen/RestaurantAnnotationView.swift
+++ redhen/RestaurantAnnotationView.swift
@@ -11,49 +11,92 @@ import CoreFoundation
 import CoreGraphics
 
 struct RestaurantAnnotationView: View {
+    var arEnabled = false
+    var notification = false
+    var open = true
+    
     var body: some View {
         VStack {
             ZStack {
-                Circle()
-                    .fill(Color(red: 229 / 255, green: 140 / 255, blue: 0 / 255))
-                    .frame(width: 40, height: 40)
-
-                Circle()
-                    .fill(Color(red: 255 / 255, green: 159 / 255, blue: 10 / 255))
-                    .frame(width: 34, height: 34)
+                if open {
+                    Circle()
+                        .fill(Color(red: 229 / 255, green: 140 / 255, blue: 0 / 255))
+                        .frame(width: 40, height: 40)
+                    
+                    Circle()
+                        .fill(Color(red: 255 / 255, green: 159 / 255, blue: 10 / 255))
+                        .frame(width: 34, height: 34)
+                } else {
+                    Circle()
+                        .fill(Color(red: 103 / 255, green: 103 / 255, blue: 103 / 255))
+                        .frame(width: 40, height: 40)
+                    
+                    Circle()
+                        .fill(Color(red: 143 / 255, green: 143 / 255, blue: 143 / 255))
+                        .frame(width: 34, height: 34)
+                }
                 
                 Image(systemName: "fork.knife")
                     .font(.system(size: 18))
                     .foregroundColor(.white)
                 
-                ZStack {
-                    RoundedRectangle(cornerRadius: 4.0, style: .circular)
-                        .fill(Color(red: 2 / 255, green: 99 / 255, blue: 0 / 255))
-                        .frame(width: 21, height: 13)
-                    
-                    RoundedRectangle(cornerRadius: 3.0, style: .circular)
-                        .fill(Color(red: 69 / 255, green: 181 / 255, blue: 0 / 255))
-                        .frame(width: 19, height: 11)
-                    
-                    Text("AR")
-                        .font(.system(size: 10))
-                        .foregroundColor(.white)
-                        .fontWeight(.semibold)
+                if arEnabled {
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 4.0, style: .circular)
+                            .fill(Color(red: 2 / 255, green: 99 / 255, blue: 0 / 255))
+                            .frame(width: 21, height: 13)
+                        
+                        RoundedRectangle(cornerRadius: 3.0, style: .circular)
+                            .fill(Color(red: 69 / 255, green: 181 / 255, blue: 0 / 255))
+                            .frame(width: 19, height: 11)
+                        
+                        Text("AR")
+                            .font(.system(size: 10))
+                            .foregroundColor(.white)
+                            .fontWeight(.semibold)
                     }
-                .offset(x: -15, y: -17)
+                    .offset(x: -15, y: -17)
+                }
+                
+                if notification && open {
+                    Circle()
+                        .fill(.red)
+                        .frame(width: 12, height: 12)
+                        .offset(x: 14, y: -14)
+                }
             }
             
-            Image(systemName: "arrowtriangle.down.fill")
-                .font(.caption)
-                .foregroundColor(Color(red: 229 / 255, green: 140 / 255, blue: 0 / 255))
-                .offset(x: 0, y: -3)
+            if open {
+                Image(systemName: "arrowtriangle.down.fill")
+                    .font(.caption)
+                    .foregroundColor(Color(red: 229 / 255, green: 140 / 255, blue: 0 / 255))
+                    .offset(x: 0, y: -3)
+            } else {
+                Image(systemName: "arrowtriangle.down.fill")
+                    .font(.caption)
+                    .foregroundColor(Color(red: 103 / 255, green: 103 / 255, blue: 103 / 255))
+                    .offset(x: 0, y: -3)
+            }
         }
     }
 }
 
 struct RestaurantAnnotationView_Previews: PreviewProvider {
     static var previews: some View {
-        RestaurantAnnotationView()
+        RestaurantAnnotationView(arEnabled: false, notification: false)
+            .previewLayout(.sizeThatFits)
+        
+        RestaurantAnnotationView(arEnabled: false, notification: true)
+            .previewLayout(.sizeThatFits)
+        
+        RestaurantAnnotationView(arEnabled: true, notification: true)
+            .previewLayout(.sizeThatFits)
+        
+        RestaurantAnnotationView(arEnabled: false, notification: true, open: false)
+            .previewLayout(.sizeThatFits)
+        
+        RestaurantAnnotationView(arEnabled: true, notification: true, open: false)
+            .previewLayout(.sizeThatFits)
     }
 }
 
